94c9ef3164f92af7f079b003129a2a4fb8a725fd
Olli Helminen <git.commits@winterburn.aleeas.com>
2025-07-03 13:23:13 +0300

    remove some consult binds.

--- lisp/keybindings.el
+++ lisp/keybindings.el
@@ -11,11 +11,11 @@
   "SPC" '(projectile-find-file :which-key "find file in project")
 
   "f" '(:ignore t :which-key "files")
-  "f f" '(counsel-find-file :which-key "find file")
+  "f f" '(find-file :which-key "find file")
   "f s" '(save-buffer :which-key "save file")
 
   "b" '(:ignore t :which-key "buffers")
-  "b b" '(counsel-switch-buffer :which-key "switch buffer")
+  "b b" '(consult-buffer :which-key "switch buffer")
   "b d" '(kill-this-buffer :which-key "kill buffer")
 
   "w" '(:ignore t :which-key "windows")
@@ -35,7 +35,7 @@
   "c d" '(lsp-ui-peek-find-definitions :which-key "definition")
   "c r" '(lsp-ui-peek-find-references :which-key "references")
   "c c" '(lsp-rename :which-key "rename")
-  "c x" '(consult-lsp-diagnostics :which-key "buffer error list")
+  "c x" '(flymake-show-buffer-diagnostics :which-key "buffer error list")
   "c X" '(flymake-show-project-diagnostics :which-key "project error list")
   "c n" '(flymake-goto-next-error :which-key "next error")
   "c p" '(flymake-goto-prev-error :which-key "previous error")
