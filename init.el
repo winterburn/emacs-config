c223e805f227bb6b3d35039645caa92b6c1fd7ae
Olli Helminen <git.commits@winterburn.aleeas.com>
2025-07-04 14:31:10 +0300

    magit blame heading color change.

--- init.el
+++ init.el
@@ -385,4 +385,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(magit-blame-heading ((t (:extend t :background "SlateBlue4" :foreground "#ff9e64")))))
